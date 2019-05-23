2c653997fa3e55f86c2934a20e870ed392ffa308
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2019-05-23 21:46:33 +0200

    Implement Android code

--- android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerPlugin.java
+++ android/src/main/java/io/inway/ringtone/player/FlutterRingtonePlayerPlugin.java
@@ -5,6 +5,7 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -16,75 +17,82 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  * FlutterRingtonePlayerPlugin
  */
 public class FlutterRingtonePlayerPlugin implements MethodCallHandler {
-  private final Context context;
-  private final RingtoneManager ringtoneManager;
-  private Ringtone ringtone;
+    private final Context context;
+    private final RingtoneManager ringtoneManager;
+    private Ringtone ringtone;
 
-  public FlutterRingtonePlayerPlugin(Context context) {
-    this.context = context;
-    this.ringtoneManager = new RingtoneManager(context);
-    this.ringtoneManager.setStopPreviousRingtone(true);
-  }
-
-  /**
-   * Plugin registration.
-   */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_ringtone_player");
-    channel.setMethodCallHandler(new FlutterRingtonePlayerPlugin(registrar.context()));
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    try {
-      Uri ringtoneUri = null;
-
-      switch (call.method) {
-        case "playAlarm":
-          ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-          break;
-        case "playNotification":
-          ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-          break;
-        case "playRingtone":
-          ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-          break;
-        case "stop":
-          if (ringtone != null) {
-            ringtone.stop();
-          }
-          break;
-        default:
-          result.notImplemented();
-      }
-
-      if (ringtoneUri != null) {
-        if (ringtone != null) {
-          ringtone.stop();
-        }
-        ringtone = ringtoneManager.getRingtone(context, ringtoneUri);
-
-        if (call.hasArgument("volume")) {
-          final double volume = call.argument("volume");
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            ringtone.setVolume((float) volume);
-          }
-        }
-
-        if (call.hasArgument("looping")) {
-          final boolean looping = call.argument("looping");
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            ringtone.setLooping(looping);
-          }
-        }
-
-        ringtone.play();
-      }
-
-      result.success(null);
-    } catch (Exception e) {
-      e.printStackTrace();
-      result.error("Exception", e.getMessage(), null);
+    public FlutterRingtonePlayerPlugin(Context context) {
+        this.context = context;
+        this.ringtoneManager = new RingtoneManager(context);
+        this.ringtoneManager.setStopPreviousRingtone(true);
     }
-  }
+
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_ringtone_player");
+        channel.setMethodCallHandler(new FlutterRingtonePlayerPlugin(registrar.context()));
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        try {
+            Uri ringtoneUri = null;
+
+            if (call.method.equals("play")) {
+                if (!call.hasArgument("android")) {
+                    return result.notImplemented();
+                }
+
+                final int kind = call.argument("android");
+
+                switch (kind) {
+                    case 1:
+                        ringtoneUri = Settings.System.DEFAULT_ALARM_ALERT_URI;
+                        break;
+                    case 2:
+                        ringtoneUri = Settings.System.DEFAULT_NOTIFICATION_URI;
+                        break;
+                    case 3:
+                        ringtoneUri = Settings.System.DEFAULT_RINGTONE_URI;
+                        break;
+                    default:
+                        result.notImplemented();
+                }
+            } else if (call.method.equals("stop")) {
+                if (ringtone != null) {
+                    ringtone.stop();
+                }
+            }
+
+            if (ringtoneUri != null) {
+                if (ringtone != null) {
+                    ringtone.stop();
+                }
+                ringtone = ringtoneManager.getRingtone(context, ringtoneUri);
+
+                if (call.hasArgument("volume")) {
+                    final double volume = call.argument("volume");
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        ringtone.setVolume((float) volume);
+                    }
+                }
+
+                if (call.hasArgument("looping")) {
+                    final boolean looping = call.argument("looping");
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        ringtone.setLooping(looping);
+                    }
+                }
+
+                ringtone.play();
+            }
+
+            result.success(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.error("Exception", e.getMessage(), null);
+        }
+    }
 }
