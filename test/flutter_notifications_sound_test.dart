77e5e62a22a8afef01040381f5c16bdb832690fd
Sebastian Podjasek <sebastian.podjasek@intelliway.pl>
2021-10-25 18:05:35 +0200

    Add missing tests

--- test/flutter_notifications_sound_test.dart
+++ test/flutter_notifications_sound_test.dart
@@ -3,7 +3,47 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('flutter_ringtone_player');
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-  // TODO Think of some test cases
+  const channelName = 'flutter_ringtone_player';
+
+  test('play', () async {
+    _initializeFakeRingtoneChannel(channelName, 'play');
+    await FlutterRingtonePlayer.play(
+      android: AndroidSounds.alarm,
+      ios: IosSounds.alarm,
+    );
+  });
+  test('playAlarm', () async {
+    _initializeFakeRingtoneChannel(channelName, 'play');
+    await FlutterRingtonePlayer.playAlarm();
+  });
+  test('playNotification', () async {
+    _initializeFakeRingtoneChannel(channelName, 'play');
+    await FlutterRingtonePlayer.playNotification();
+  });
+  test('playRingtone', () async {
+    _initializeFakeRingtoneChannel(channelName, 'play');
+    await FlutterRingtonePlayer.playRingtone();
+  });
+
+  test('stop', () async {
+    _initializeFakeRingtoneChannel(channelName, 'stop');
+
+    await FlutterRingtonePlayer.stop();
+  });
+}
+
+void _initializeFakeRingtoneChannel(String channelName, String expectedMethod) {
+  const standardMethod = StandardMethodCodec();
+
+  TestDefaultBinaryMessengerBinding.instance!.defaultBinaryMessenger
+      .setMockMessageHandler(channelName, (ByteData? message) async {
+    final methodCall = standardMethod.decodeMethodCall(message);
+    if (methodCall.method == expectedMethod) {
+      return standardMethod.encodeSuccessEnvelope(null);
+    } else {
+      fail("Expected '$expectedMethod' method to be called");
+    }
+  });
 }
