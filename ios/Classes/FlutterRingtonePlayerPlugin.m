be18cd3dd51a788d75316eb2a41e140528bcee1b
GeorgeAmgad <georgeamgad@innuva.com>
2022-04-04 17:25:26 +0200

    Added support for custom ringtones from assets.

--- ios/Classes/FlutterRingtonePlayerPlugin.m
+++ ios/Classes/FlutterRingtonePlayerPlugin.m
@@ -3,18 +3,39 @@
 
 
 @implementation FlutterRingtonePlayerPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-    FlutterMethodChannel* channel = [FlutterMethodChannel
-                                     methodChannelWithName:@"flutter_ringtone_player"
-                                     binaryMessenger:[registrar messenger]];
-    FlutterRingtonePlayerPlugin* instance = [[FlutterRingtonePlayerPlugin alloc] init];
+NSObject <FlutterPluginRegistrar> *pluginRegistrar = nil;
+
++ (void)registerWithRegistrar:(NSObject <FlutterPluginRegistrar> *)registrar {
+    pluginRegistrar = registrar;
+    FlutterMethodChannel *channel = [FlutterMethodChannel
+            methodChannelWithName:@"flutter_ringtone_player"
+                  binaryMessenger:[registrar messenger]];
+    FlutterRingtonePlayerPlugin *instance = [[FlutterRingtonePlayerPlugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
     if ([@"play" isEqualToString:call.method]) {
-        NSNumber *soundId = (NSNumber *)call.arguments[@"ios"];
-        AudioServicesPlaySystemSound([soundId integerValue]);
+        SystemSoundID soundId = nil;
+        CFURLRef soundFileURLRef = nil;
+
+        if (call.arguments[@"uri"] != nil) {
+            NSString *key = [pluginRegistrar lookupKeyForAsset:call.arguments[@"uri"]];
+            NSURL *path = [[NSBundle mainBundle] URLForResource:key withExtension:nil];
+            soundFileURLRef = CFBridgingRetain(path);
+            AudioServicesCreateSystemSoundID(soundFileURLRef, &soundId);
+        }
+
+        // The iosSound overrides fromAsset if exists
+        if (call.arguments[@"ios"] != nil) {
+            soundId = (SystemSoundID) [call.arguments[@"ios"] integerValue];
+        }
+
+        AudioServicesPlaySystemSound(soundId);
+        if (soundFileURLRef != nil) {
+            CFRelease(soundFileURLRef);
+        }
+
         result(nil);
     } else if ([@"stop" isEqualToString:call.method]) {
         result(nil);
